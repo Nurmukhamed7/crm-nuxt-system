b9e66a7b7f4d0507d9383beaf406a3532c67fbe1
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-07 18:24:54 +0100

    Фикс настроек по цветам

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,16 +1,14 @@
 <template>
-	<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+	<aside class="px-5 py-8 bg-secondary h-full relative w-full">
 		<NuxtLink to="/" class="mb-10 block">
 			<NuxtImg
 				src="/logo.svg"
 				alt="logo"
-				width="120px"
+				width="100px"
 				class="mx-auto"
 			></NuxtImg>
 		</NuxtLink>
-		<button
-			class="absolute text-primary top-2 right-3 transition-colors hover:text-purple-400"
-		>
+		<button class="absolute top-2 right-3 transition-colors hover:text-primary">
 			<Icon name="line-md:logout" size="20"></Icon>
 		</button>
 		<LayoutMenu></LayoutMenu>
