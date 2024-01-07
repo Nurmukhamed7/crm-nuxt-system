6423015c67676056350744bc4539037fbbf42c32
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-07 17:24:05 +0100

    Создал appwrite.ts и app.constants.ts

---
+++ app.constants.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = '659abbe00a6830b424a8'
+export const DB_ID = 'crm-base'
+export const COLLECTION_DEALS = 'deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
