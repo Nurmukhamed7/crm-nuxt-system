6ed6b1ba1af2a801b04f85a5067a08c6372faab9
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-08 15:42:21 +0100

    Добавляем Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,9 +17,13 @@ export default defineNuxtConfig({
 			},
 		],
 		'nuxt-icon',
+		'@pinia/nuxt',
 	],
 	shadcn: {
 		prefix: 'Ui',
 		componentDir: './components/ui',
+	},
+	pinia: {
+		storesDirs: ['./stores/**'],
 	},
 })
