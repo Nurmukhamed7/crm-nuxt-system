79d9ecb77055f99321ada6e3ba0ef3fcca426b41
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-09 20:07:26 +0100

    Добавил стили для Card и useHead для index.vue

--- components/ui/card/Card.vue
+++ components/ui/card/Card.vue
@@ -2,22 +2,45 @@
 import { cn } from '@/lib/utils'
 
 const props = defineProps({
-  class: {
-    type: String,
-    default: '',
-  },
+	class: {
+		type: String,
+		default: '',
+	},
 })
 </script>
 
 <template>
-  <div
-    :class="
-      cn(
-        'rounded-lg border bg-card text-card-foreground shadow',
-        props.class,
-      )
-    "
-  >
-    <slot />
-  </div>
+	<div
+		:class="
+			cn(
+				'rounded-lg border bg-card text-card-foreground animation border-transparent transition-colors hover:border-[#919191]',
+				props.class
+			)
+		"
+	>
+		<slot />
+	</div>
 </template>
+
+<style scoped>
+@keyframes show {
+	from {
+		@apply border-[#919191];
+		transform: scale(0.5) translateY(-30px);
+		opacity: 0.4;
+	}
+
+	90% {
+		@apply border-[#919191];
+	}
+
+	to {
+		@apply border-transparent;
+		transform: scale(1) translateY(0);
+		opacity: 1;
+	}
+}
+.animation {
+	animation: show 0.3s ease-in-out;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+	title: 'Home | CRM System',
+})
+</script>
 
 <template>
 	<div class="p-10">
 		<h1 class="font-bold text-2xl mb-10">CRM System by Nurmukhamed</h1>
-		<UiCard />
+		<div>
+			<UiCard class="mb-3" draggable="true">
+				<UiCardHeader role="button">asd</UiCardHeader>
+				<UiCardContent>asasdasd</UiCardContent>
+				<UiCardFooter>asd</UiCardFooter>
+			</UiCard>
+		</div>
 	</div>
 </template>
 
