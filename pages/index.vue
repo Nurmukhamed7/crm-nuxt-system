8b415956a44e384faef33ccbe1eea645d2868d15
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-09 17:56:29 +0100

    создал card компонент

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
 	<div class="p-10">
 		<h1 class="font-bold text-2xl mb-10">CRM System by Nurmukhamed</h1>
+		<UiCard />
 	</div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
